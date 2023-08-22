b28f4d24b2f1119afe379b53dea2d77dc07d5387
huhuiyu <1069306849@qq.com>
2023-08-22 11:23:04 +0800

    多仓库推送脚本

---
+++ push.ps1
@@ -0,0 +1,7 @@
+# github =====> git remote add github git@github.com:huhuiyu/template-vue.git
+# jihulab =====> git remote add jihulab git@jihulab.com:DarkKnight/template-vue.git
+# gitee =====> git remote add gitee git@gitee.com:huhuiyu/template-vue.git
+git pull
+git push
+git push jihulab
+git push gitee
