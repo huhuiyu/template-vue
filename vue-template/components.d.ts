68308605c4cf832b8fa430482adaafd2afceae00
huhuiyu <1069306849@qq.com>
2023-09-05 09:45:02 +0800

    build脚本添加虚拟目录和跳过eslint的部分

--- vue-template/components.d.ts
+++ vue-template/components.d.ts
@@ -16,7 +16,6 @@ declare module 'vue' {
     ElInput: typeof import('element-plus/es')['ElInput']
     ElPagination: typeof import('element-plus/es')['ElPagination']
     ElRow: typeof import('element-plus/es')['ElRow']
-    ElSelect: typeof import('element-plus/es')['ElSelect']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
     ElTag: typeof import('element-plus/es')['ElTag']
